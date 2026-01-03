e973eeea60493a384f66d83535faa38050590cf3
Stanislaw wielgus <153013505+h852852@users.noreply.github.com>
2026-01-04 00:47:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ UUID = os.environ.get("UUID", "fd80f56e-93f3-4c85-b2a8-c77216c369a7")  # UUID
 NEZHA_SERVER = os.environ.get("NEZHA_SERVER", "")  # 哪吒面板域名
 NEZHA_PORT = os.environ.get("NEZHA_PORT", "")  # 哪吒端口
 NEZHA_KEY = os.environ.get("NEZHA_KEY", "")  # 哪吒密钥
-ARGO_DOMAIN = os.environ.get("ARGO_DOMAIN", "ss.ukiss.me")  # Argo固定域名
-ARGO_AUTH = os.environ.get("ARGO_AUTH", "export TOK=${TOK:-"eyJhIjoiZWU4NWQ5YmQwNTI0OThhNmIwNWY0N2NjZmQ1YjM1MzIiLCJ0IjoiNTdmOTMyYjAtZTljNi00MmFlLWIzNzMtYjJkMDQ3YjNlNmI1IiwicyI6Ik56SXlOak0zTldRdE1HUXlaaTAwWkRKaUxXSXhZbU10WVRJMFltVmtZVEJpT0RZMyJ9")  # Argo密钥
+ARGO_DOMAIN = os.environ.get("ARGO_DOMAIN", "g.rongt.ggff.net")  # Argo固定域名
+ARGO_AUTH = os.environ.get("ARGO_AUTH", "export TOK=${TOK:-"eyJhIjoiZWU4NWQ5YmQwNTI0OThhNmIwNWY0N2NjZmQ1YjM1MzIiLCJ0IjoiZGNkNTZiNWItZTdlMi00MmY0LWFjZWYtZDdmZTliMTI1OTFmIiwicyI6IlpERTNaRFJrTUdZdFptRXdaaTAwTVdNekxXSXhaRGt0T0Rka1ltVTFOR0l6T1dObCJ9")  # Argo密钥
 ARGO_PORT = int(os.environ.get("PORT", "8001"))  # Argo监听端口
 CFIP = os.environ.get("CFIP", "cf.090227.xyz")  # 优选ip
 CFPORT = int(os.environ.get("CFPORT", "443"))  # 优选端口
